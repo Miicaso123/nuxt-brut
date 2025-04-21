970fcd4d07ee2ea6e0b7cf70483bf39e09ecc6d9
Kurmanay <ttkurmanai@gmail.com>
2025-04-21 13:05:50 +0500

    delete file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  ssr: false,
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [tailwindcss()],
